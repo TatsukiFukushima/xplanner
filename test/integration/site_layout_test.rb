5ada052b774b000d051bb2a256dd8b1fe429340e
EC2 Default User <ec2-user@ip-172-31-28-126.ap-southeast-1.compute.internal>
2018-09-20 05:58:25 +0000

    Implement basic login

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'landing_page/home'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", '#'
-    assert_select "a[href=?]", '#'
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
   end 
 end
