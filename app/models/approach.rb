e24ad6d41c7625af37b21a61ec8407eaf13e0892
EC2 Default User <ec2-user@ip-172-31-28-126.ap-southeast-1.compute.internal>
2018-10-03 16:20:07 +0000

    Add a memo function to the goals and approach

--- app/models/approach.rb
+++ app/models/approach.rb
@@ -3,6 +3,7 @@ class Approach < ApplicationRecord
   ranks :row_order, with_same: :short_term_goal_id 
   belongs_to :short_term_goal
   has_one :deadline, as: :due_date, dependent: :destroy, inverse_of: :due_date
+  has_one :memo, as: :memoable, dependent: :destroy
   accepts_nested_attributes_for :deadline
   validates :content, presence: true, length: { maximum: 255 }
   validates :effectiveness, presence: true 
