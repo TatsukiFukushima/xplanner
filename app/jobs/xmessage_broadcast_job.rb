358ad6da02537522597f6427fed8e4f842acd7e4
EC2 Default User <ec2-user@ip-172-31-28-126.ap-southeast-1.compute.internal>
2018-10-11 13:21:28 +0000

    Fix a type in xroom.coffee

--- app/jobs/xmessage_broadcast_job.rb
+++ app/jobs/xmessage_broadcast_job.rb
@@ -2,7 +2,7 @@ class XmessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(xmessage)
-    ActionCable.server.broadcast 'xroom_channel', xmessage:
+    ActionCable.server.broadcast "xroom_channel_#{xmessage.xroom_id}", xmessage:
       render_xmessage(xmessage)
   end 
   
--- app/models/xroom.rb
+++ app/models/xroom.rb
@@ -1,4 +1,6 @@
 class Xroom < ApplicationRecord
   belongs_to :user
   has_many :xmessages, dependent: :destroy
+  validates :category, presence: true
+  validates :description, presence: true
 end
