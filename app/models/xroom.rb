47165f1329567e25252d8699e7f2bae902daa343
EC2 Default User <ec2-user@ip-172-31-28-126.ap-southeast-1.compute.internal>
2018-10-18 07:43:04 +0000

    Add xroom search function

--- app/models/xroom.rb
+++ app/models/xroom.rb
@@ -9,4 +9,27 @@ class Xroom < ApplicationRecord
   def user_count
     Subscription.where(xroom_id: self.id).count
   end
+  
+  
+  # 検索機能のためのメソッド
+  def self.search(search: nil, x_attr: nil, order: nil)
+    if search.present?
+      case x_attr
+      when 'name'
+        tmp = Xroom.where(['name LIKE ?', "%#{search}%"])
+      when 'category'
+        tmp = Xroom.where(['category LIKE ?', "%#{search}%"]) 
+      when 'description'
+        tmp = Xroom.where(['description LIKE ?', "%#{search}%"])
+      end 
+    else
+      tmp = Xroom.all
+    end
+    if order == 'asc'
+      tmp = tmp.sort { |a, b| a.subscriptions.count <=> b.subscriptions.count }
+    elsif order == 'desc' 
+      tmp = tmp.sort { |a, b| b.subscriptions.count <=> a.subscriptions.count }
+    end
+    return tmp
+  end
 end
